7ecb54837fbacc994024d9d65eca848259878967
Sean H <schordines@gmail.com>
2025-11-04 15:29:34 -0500

    changes to the image processing

--- app/src/main/java/com/ungcsci/paintbynumber/ImageProcessor.java
+++ app/src/main/java/com/ungcsci/paintbynumber/ImageProcessor.java
@@ -1,51 +1,27 @@
 package com.ungcsci.paintbynumber;
 
-import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.net.Uri;
-import android.os.Build;
-import androidx.annotation.RequiresApi;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Random;
 
 public class ImageProcessor {
     private final Context context;
     private Bitmap original_image;
     private Bitmap posterized_image;
-    private Color[] palette;
-    private Color[][] color_grid;
+    private int[] palette;
+    private int[][] color_grid;
 
-    public ImageProcessor(Context context) {
+    public ImageProcessor(Context context, Uri uri, int logical_size, int num_colors) {
         this.context = context;
-    }
 
-    public boolean loadImageFromURI(Uri uri) {
-        original_image = null;
-        ContentResolver contentResolver = context.getContentResolver();
-        InputStream inputStream = null;
-
-        try {
-            inputStream = contentResolver.openInputStream(uri);
-            if (inputStream == null) {return false;}
-
-            original_image = BitmapFactory.decodeStream(inputStream);
-        } catch (FileNotFoundException e) {e.printStackTrace();}
-
-        return original_image != null;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public boolean loadImageFromURI(Uri uri, int logical_size, int num_colors) {
         loadImageFromURI(uri);
-        if (original_image == null) return false;
-
         posterizeImage(logical_size, num_colors);
-        return posterized_image != null;
     }
 
     public Bitmap getOriginalImage() {
@@ -53,22 +29,60 @@ public class ImageProcessor {
     }
 
     public Bitmap getPosterizedImage(int display_size) {
-        if (posterized_image == null) return null;
-        return Bitmap.createScaledBitmap(posterized_image, display_size, display_size, false);
+        if (original_image == null) return null;
+
+        if (posterized_image != null) {
+            if (display_size < posterized_image.getWidth()) return posterized_image;
+            // scale up to the display size using nearest neighbor interpolation to preserve hard edges
+            return Bitmap.createScaledBitmap(posterized_image, display_size, display_size, false);
+        }
+
+        return null;
     }
 
-    public Color[] getPalette() {
+    public int[] getPalette() {
+        if (posterized_image == null) return null;
         return palette;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
+    public int[][] getColorGrid() {
+        if (posterized_image == null) return null;
+        if (color_grid != null) return color_grid;
+
+        int width = posterized_image.getWidth();
+        int height = posterized_image.getHeight();
+
+        int[] pixels = new int[width*height];
+        posterized_image.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                color_grid[x][y] = pixels[y * width + x];
+            }
+        }
+        return color_grid;
+    }
+
+    private void loadImageFromURI(Uri uri) {
+        original_image = null;
+        ContentResolver contentResolver = context.getContentResolver();
+        InputStream inputStream = null;
+
+        try {
+            inputStream = contentResolver.openInputStream(uri);
+            if (inputStream == null) {return;}
+
+            original_image = BitmapFactory.decodeStream(inputStream);
+        } catch (FileNotFoundException e) {e.printStackTrace();}
+    }
+
     private void posterizeImage(int logical_size, int num_colors) {
         // crop image to square
         Bitmap logical_image = cropToSquare(original_image);
-        // scale to logical sBitmap logical_imageize using cubic
+        // scale to logical size using bilinear interpolation
         logical_image = Bitmap.createScaledBitmap(logical_image, logical_size, logical_size, true);
         // use k-means to identify best colors
-        kMeansPaletteReduction(logical_image, num_colors);
+        posterized_image = kMeansPaletteReduction(logical_image, num_colors);
     }
 
     private Bitmap cropToSquare(Bitmap source) {
@@ -82,8 +96,13 @@ public class ImageProcessor {
         return Bitmap.createBitmap(source, x_offset, y_offset, size, size);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void kMeansPaletteReduction(Bitmap logical_image, int num_colors) {
+    private Bitmap denoiseBilateral(Bitmap logical_image, float radius, float sigmaColor, float sigmaSpace) {
+        // for each pixel
+        // get the neighbors in the radius
+        // compute 
+    }
+
+    private Bitmap kMeansPaletteReduction(Bitmap logical_image, int num_colors) {
         int width = logical_image.getWidth();
         int height = logical_image.getHeight();
         int num_pixels = width*height;
@@ -91,7 +110,7 @@ public class ImageProcessor {
         int[] pixels = new int[num_pixels];
         logical_image.getPixels(pixels, 0, width, 0, 0, width, height);
 
-        float[][] color_data = new float[num_pixels][3];
+        int[][] color_data = new int[num_pixels][3];
         for (int i = 0; i < num_pixels; i++) {
             int color = pixels[i];
             color_data[i][0] = (color >> 16) & 0xFF; // red channel
@@ -99,7 +118,7 @@ public class ImageProcessor {
             color_data[i][2] = color & 0xFF; // blue channel
         }
 
-        float[][] clusters = new float[num_colors][3];
+        int[][] clusters = new int[num_colors][3];
         Random rand = new Random(314159265);
         for (int k = 0; k < num_colors; k++) {
             int random_color_index = rand.nextInt(num_pixels);
@@ -107,6 +126,7 @@ public class ImageProcessor {
         }
 
         int[] best_cluster_indexes = new int[num_pixels];
+        Arrays.fill(best_cluster_indexes, -1);
 
         int max_iterations = 20;
         boolean modified = true;
@@ -114,15 +134,15 @@ public class ImageProcessor {
             modified = false;
 
             for (int i = 0; i < num_pixels; i++){
-                float min_distance = Float.MAX_VALUE;
+                int min_distance = Integer.MAX_VALUE;
                 int best_cluster_index = 0;
 
                 for (int k = 0; k < num_colors; k++) {
-                    float delta_red = color_data[i][0] - clusters[k][0];
-                    float delta_green = color_data[i][1] - clusters[k][1];
-                    float delta_blue = color_data[i][2] - clusters[k][2];
+                    int delta_red = color_data[i][0] - clusters[k][0];
+                    int delta_green = color_data[i][1] - clusters[k][1];
+                    int delta_blue = color_data[i][2] - clusters[k][2];
 
-                    float current_distance =
+                    int current_distance =
                             (delta_red * delta_red) +
                             (delta_green * delta_green) +
                             (delta_blue * delta_blue);
@@ -139,7 +159,7 @@ public class ImageProcessor {
                 }
             }
 
-            float[][] new_clusters = new float[num_colors][3];
+            int[][] new_clusters = new int[num_colors][3];
             int[] cluster_counts = new int[num_colors];
 
             for (int i = 0; i < num_pixels; i++) {
@@ -162,22 +182,21 @@ public class ImageProcessor {
             }
         }
 
-        palette = new Color[num_colors];
+        palette = new int[num_colors];
         for (int k = 0; k < num_colors; k++) {
-            float red = clusters[k][0] / 255f;
-            float green = clusters[k][1] / 255f;
-            float blue = clusters[k][2] / 255f;
-            palette[k] = Color.valueOf(red, green, blue);
+            int red = (int) clusters[k][0];
+            int green = (int) clusters[k][1];
+            int blue = (int) clusters[k][2];
+            palette[k] = (0xFF << 24) + (red << 16) + (green << 8) + (blue);
         }
 
         int[] new_pixels = new int[num_pixels];
         for (int i = 0; i < num_pixels; i++) {
-            int palette_index = best_cluster_indexes[i];
-            Color new_color = palette[palette_index];
-            new_pixels[i] = new_color.toArgb();
+            new_pixels[i] = palette[best_cluster_indexes[i]];
         }
 
-        posterized_image = Bitmap.createBitmap(width, height, logical_image.getConfig());
-        posterized_image.setPixels(new_pixels, 0, width, 0, 0, width, height);
+        Bitmap color_reduced_image = Bitmap.createBitmap(width, height, logical_image.getConfig());
+        color_reduced_image.setPixels(new_pixels, 0, width, 0, 0, width, height);
+        return color_reduced_image;
     }
 }
